d586808d9c7405962f8e4b9ec736b72c72703fc3
rushirrj <rushikeshdjrrj@gmail.com>
2023-07-26 21:55:59 +0530

    Create Open Vacancies

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Openvacancies from "../components/Openvacancies";
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Openvacancies />
       <Footer/>
     </>
   );
