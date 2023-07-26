98e11a67004b74edda2972c1ca3410c42dc3e0f0
rushirrj <rushikeshdjrrj@gmail.com>
2023-07-26 22:03:14 +0530

    Create Static Components

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,16 @@ import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Openvacancies from "../components/Openvacancies";
+import EQbeatsIQ from "../components/EQbeatsIQ";
+import BeBestYou from "../components/BeBestYou";
 export default function Home() {
   return (
     <>
       <Navbar />
+      <EQbeatsIQ />
+      <BeBestYou />
       <Openvacancies />
-      <Footer/>
+      <Footer />
     </>
   );
 }
