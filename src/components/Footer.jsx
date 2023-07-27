f2d77a92f4b147066696960535314504366cfbca
rushirrj <rushikeshdjrrj@gmail.com>
2023-07-27 20:31:43 +0530

    Add Ahead Logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="mt-4 border-gray-300  py-12 px-12 border-t">
-      <p className="text-xl font-bold text-violet-700 text-center">LOGO</p>
+      <p className="text-xl font-bold text-violet-700 flex justify-center">
+        <Image width={60} height={60} alt="" src="/ahead-logo.png" />
+      </p>
       <p className="text-xl font-bold text-violet-700 text-center">ahead</p>
       <div className="flex gap-8 items-center justify-center mt-5 font-semibold">
         <div className="">AugustraBe 26, 10117 Berlin</div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function Navbar() {
   return (
     <div className="fixed top-[0] left-[0]  bg-white z-[100]">
       <header className="flex  justify-between h-20 items-center py-3 px-10 w-screen">
-        <div className="">Logo</div>
+        <div className=""><Image width={50} height={50} alt="" src="/ahead-logo.png" /></div>
         <div className="">
           <ul className="flex gap-6 items-center">
             <li className="cursor-pointer font-semibold ">Emotions</li>
